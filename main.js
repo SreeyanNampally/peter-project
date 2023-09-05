5e17857b635488b984578cb2fc10bc6dfd23eb55
Sreeyan Nampally <98562308+SreeyanNampally@users.noreply.github.com>
2023-09-05 17:58:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup(){
+    canvas = createCanvas(500,500)
+    canvas.center()
+    video = createCapture(VIDEO);
+}
+
+function draw(){
+    textSize(64)
+    text("peter", 100, 100)
+}
